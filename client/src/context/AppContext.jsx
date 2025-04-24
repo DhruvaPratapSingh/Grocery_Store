7613474f64aecb05c5c15be523ee64cabb296edd
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2025-04-24 22:41:40 +0530

    feat: integrate Tailwind CSS and set up AppContext
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Refactored index.css to utilize Tailwind CSS and removed custom styles.
    - Updated main.jsx to include BrowserRouter and AppContextProvider for routing and state management.
    - Created AppContext for managing user state and navigation within the application.

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+export const AppContext = createContext();
+export const AppContextProvider = ({ children }) => {
+    const navigate=useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller,setIsSeller]=useState(false);
+    const value={navigate,user,setUser,isSeller,setIsSeller};
+  return <AppContext.Provider value={value}>
+  {children}
+  </AppContext.Provider>
+}
+export const useAppContext = () => {
+    const context = useContext(AppContext);
+    if (!context) {
+        throw new Error("useAppContext must be used within an AppContextProvider");
+    }
+    return context;
+}
