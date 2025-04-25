dc0719abd0ad8e5b3aa0965f007cf66aa8419bb3
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2025-04-25 19:41:11 +0530

    feat: enhance ProductCategory and ProductDetails pages; update grid layout and add related products section

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -21,7 +21,7 @@ const ProductCategory = () => {
       }
       {
         filterProducts.length>0 ? (
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6 mt-6'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
                 {filterProducts.map((product)=>( 
                     <ProductCard key={product._id} product={product}/>
 
--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -3,6 +3,7 @@ import { useAppContext } from '../context/AppContext';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { assets } from '../assets/assets';
+import { ProductCard } from '../components/ProductCard';
 const ProductDetails = () => {
     const {products,navigate,currency,addToCart}=useAppContext();
     const {id}=useParams();
@@ -83,6 +84,21 @@ const ProductDetails = () => {
                         </button>
                     </div>
                 </div>
+            </div>
+            {/* related products */}
+            <div className='flex flex-col items-center mt-20'> 
+           <div className='flex flex-col items-center w-max'>
+            <p className='text-2xl font-medium'>Related Products</p>
+            <div className='w-20 h-0.5 bg-primary rounded-full mt-2'></div>
+           </div>
+           <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6 w-full'>
+            {
+                relatedProducts.filter((product)=>product.inStock).map((product, index) => (
+                    <ProductCard key={index} product={product} />
+                )
+            )}
+           </div>
+           <button onClick={()=>{navigate('/products'); scrollTo(0,0)}} className='mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-primary hover:bg-primary/30 transition'>See More</button>
             </div>
         </div>
     );
