26e56e4173c38452b22314c9835198443460ff56
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2025-04-27 12:55:23 +0530

    feat: add MyOrders page and integrate it into App; update routing for order management

---
+++ client/src/pages/MyOrders.jsx
@@ -0,0 +1,34 @@
+import React,{useEffect, useState} from 'react'
+import { useAppContext } from '../context/AppContext';
+import { dummyOrders } from '../assets/assets';
+
+const MyOrders = () => {
+    const [myOrders, setMyOrders] = useState([]);
+    const {currency}=useAppContext();
+
+    const fetchMyOrders = async () => {
+        setMyOrders(dummyOrders);
+    }
+    useEffect(() => {
+        fetchMyOrders();
+    }, []);
+  return (
+   <div className="mt-16 pb-16">
+     <div className='flex flex-col items-end w-max mb-8'>
+        <p className='text-2xl font-medium uppercase'>My Orders</p>
+        <div className='w-16 h-0.5 bg-primary rounded-full'></div>
+      </div>
+      {myOrders.map((order, index) => (
+           <div>
+           <p>
+            <span>OrderId : {order._id}</span>
+            <span>Payment : {order.paymentType}</span>
+            <span>Date :</span>
+           </p>
+           </div> 
+           ))}
+   </div>
+  )
+}
+
+export default MyOrders
