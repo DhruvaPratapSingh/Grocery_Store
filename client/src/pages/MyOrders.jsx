03a58cc01d6d25649a1dfae75d2542fea57285be
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2025-04-28 15:37:48 +0530

    feat: enhance MyOrders page layout; improve order and item display structure

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -19,12 +19,35 @@ const MyOrders = () => {
         <div className='w-16 h-0.5 bg-primary rounded-full'></div>
       </div>
       {myOrders.map((order, index) => (
-           <div>
-           <p>
+           <div key={index} className='border border-gray-300 rounded-lg p-4 py-5 max-w-4xl mb-10'>
+           <p className="flex justify-between md:items-center text-gray-400 md:font-medium max-md:flex-col">
             <span>OrderId : {order._id}</span>
             <span>Payment : {order.paymentType}</span>
-            <span>Date :</span>
+            <span>Total Amount : {currency}{order.amount}</span>
            </p>
+           {order.items.map((item, index) => (
+            <div key={index} className={`relative bg-white text-gray-500/70 ${order.items.length !=index+1 && "border-b"} border-gray-300 flex flex-col md:flex-row md:items-center py-5 md:gap-16 w-full max-w-4xl rounded-lg  justify-between`}>
+                <div className='flex items-center mb-4 md:mb-0'>
+                    <div className="bg-primary/10 p-4 rounded-lg">
+                        <img src={item.product.image[0]} alt="item image" className='w-16 h-16' />
+                    </div>
+                    <div className='ml-4'>
+                    <h2 className='text-xl font-medium text-gray-800'>{item.product.name}</h2>
+                    <p>Category : {item.product.category}</p>
+                    </div>
+                </div>
+
+                <div className='text-primary font-medium text-lg'>
+                <p>Quantity : {item.quantity || '1'}</p>
+                <p>status: {item.status}</p>
+                <p>Date : {new Date(order.createdAt).toLocaleDateString()}</p>
+                </div>
+                <p className='text-primary font-medium text-lg'>
+                  Amount : {currency}{item.product.offerPrice * item.quantity}
+                </p>
+            </div>
+
+      ))}
            </div> 
            ))}
    </div>
