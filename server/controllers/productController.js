a2abeb28bd357424f4ff4e61aa7e6d14ca312441
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2025-05-03 23:06:00 +0530

    feat: implement product controller with CRUD endpoints

---
+++ server/controllers/productController.js
@@ -0,0 +1,17 @@
+
+// post /api/product/add
+export const addProduct =async(req,res)=>{
+
+}
+// get /api/product/list
+export const productList =async(req,res)=>{
+
+}
+// get /api/product/id
+export const productById =async(req,res)=>{
+
+}
+//  /api/product/stock
+export const changeStock =async(req,res)=>{
+
+}
